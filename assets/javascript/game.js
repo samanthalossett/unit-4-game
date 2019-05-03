fde4237f024a8868810b25de48afa5ccbe86bd02
SamanthaLossett <samanthalossett@gmail.com>
2019-05-03 15:27:18 -0700

    with the crystals adding together & a win/lose situation, crystals still not individually working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,18 @@
-
-
     var compNumber;
     var jewelNumbers;
     var wins= "";
     var losses="";
+    var previousNum= 0;
+   
 
-// make the ID "goal" produce a random number between 19 and 100 or whatever//
+// make the ID "goal" produce a random number between 19 and 100 or whatever **THIS IS WORKING**//
 compNumber=Math.floor((Math.random() * 100) + 19);
 console.log(compNumber);
 $("#goal").html("Get to this number:" +" "+ compNumber);
 
 
-
-//make each individual crystal get a random number betwween 1 and 12 when CLICKED on//
+for (var i=0; i < 4; i++){
+//make each individual crystal get a random number betwween 1 and 12 when CLICKED on ??click works but each rystal gets the same number???** *//
     var crystalRandomNumber= Math.floor((Math.random() * 12) + 1);
     console.log(crystalRandomNumber);
 
@@ -20,10 +20,23 @@ $("#goal").html("Get to this number:" +" "+ compNumber);
         crystals.attr({
             "data-random": crystalRandomNumber
         });
-
+    }
 
 $(".crystals").on("click", function (){
+
 console.log($(this).attr("data-random"));
+
+var num= parseInt ($(this).attr("data-random"));
+
+previousNum += num;
+
+if(previousNum > compNumber){
+    console.log("You Lose")
+}
+else if(previousNum === compNumber) {
+    console.log("You win!");
+}
+console.log(previousNum);
 
 });
     
