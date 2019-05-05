f41dbdfe4db5f3ada349595dca7e65c2120b5cdc
SamanthaLossett <samanthalossett@gmail.com>
2019-05-05 13:39:02 -0700

    total score working, jjewels working, alert you lose working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,95 @@
     var compNumber;
     var jewelNumbers;
-    var wins= "";
-    var losses="";
+    var wins= 0;
+    var losses= 0;
     var previousNum= 0;
-   
+    var jewel1;
+    var jewel2;
+    var jewel3;
+    var jewel4;
+    var score= 0;
 
-// make the ID "goal" produce a random number between 19 and 100 or whatever **THIS IS WORKING**//
-compNumber=Math.floor((Math.random() * 100) + 19);
-console.log(compNumber);
-$("#goal").html("Get to this number:" +" "+ compNumber);
+    $( document ).ready(function() {
+        console.log( "ready!" );
 
+var showJewelScore= function(){
+    $("#total-score").text("Your total score: " + score);
+}
 
-for (var i=0; i < 4; i++){
-//make each individual crystal get a random number betwween 1 and 12 when CLICKED on ??click works but each rystal gets the same number???** *//
-    var crystalRandomNumber= Math.floor((Math.random() * 12) + 1);
-    console.log(crystalRandomNumber);
-
-    var crystals= $(".crystals");
-        crystals.attr({
-            "data-random": crystalRandomNumber
-        });
+var returnRandomNumber= function(){
+    return Math.floor((Math.random() * 12) + 1); 
+}
+var checkScore= function(){
+    if(score > compNumber){  //if the added number is larger than the goal number, you lose //
+        alert("You Lose");
+       // losses--;
+    //document.getElementById("Losses:").innerHTML= "Losses: " + losses;
+    //previousNum= 0;
+    //resetStartGame();
+      
     }
-
-$(".crystals").on("click", function (){
-
-console.log($(this).attr("data-random"));
-
-var num= parseInt ($(this).attr("data-random"));
-
-previousNum += num;
-
-if(previousNum > compNumber){
-    console.log("You Lose")
 }
-else if(previousNum === compNumber) {
-    console.log("You win!");
-}
-console.log(previousNum);
+//start game here//
+var resetStartGame = function (){
 
-});
-    
+        // make the ID "goal" produce a random number between 19 and 100 or whatever **THIS IS WORKING**//
+        compNumber=Math.floor((Math.random() * 100) + 19);
+        console.log(compNumber);
+        $("#goal").html("Get to this number:" +" "+ compNumber);
+
+        //make each individual crystal get a random number betwween 1 and 12 when CLICKED on     
+            jewel1=returnRandomNumber();
+            jewel2=returnRandomNumber();
+            jewel3=returnRandomNumber();
+            jewel4=returnRandomNumber();
+            console.log(jewel1, jewel2, jewel3, jewel4);
+}
+   resetStartGame(); // calling out the function of starting the game //
+
+    $("#jewelry1").on("click", function(){//making the crystals clickable, like buttons but not//
+        console.log("its working");
+        score= score + jewel1;
+        console.log(score);
+        showJewelScore();
+        checkScore();
+
+    })
+    $("#jewelry2").on("click", function(){//making the crystals clickable, like buttons but not//
+        console.log("its working");
+        score= score + jewel2;
+        console.log(score);
+        showJewelScore();
+        checkScore();
+    })
+    $("#jewelry3").on("click", function(){//making the crystals clickable, like buttons but not//
+        console.log("its working");
+        score= score + jewel3;
+        console.log(score);
+        showJewelScore();
+        checkScore();
+    })
+    $("#jewelry4").on("click", function(){//making the crystals clickable, like buttons but not//
+        console.log("its working");
+        score= score + jewel4;
+        console.log(score);
+        showJewelScore();
+        checkScore();
+    })
+
+
+// else if(score=== compNumber) {    //otherwise, if theyre the same, then you win//
+//     console.log("You win!");
+//     wins++;
+//     document.getElementById("Wins:").innerHTML= "Wins: " + wins;
+//      previousNum= 0;
+//      resetStartGame();
+// }
+//console.log(previousNum);
+
 
 
 //update the "total score" each time a jewel is clicked//
-
-
-//if the total score === goal score, then say "you win" and add++ to the wins column//
-
-
-//if the total score != goal score, then say "you lose" and add-- to the loss column//
-
-
 //when === or != happens, restart the game.//
 
-
+});
 
